cb8d71f28dbab36a7ae175bfc559a353817c7d1c
John Park <jwpark417@gmail.com>
2016-04-21 20:04:56 -0400

    Add dropbox image support

--- pop_flix/app/models/movie.rb
+++ pop_flix/app/models/movie.rb
@@ -3,6 +3,14 @@ class Movie < ActiveRecord::Base
   belongs_to :user
   has_many :reviews
 
-  has_attached_file :image, styles: { medium: "400x600#" }
+  if Rails.env.development?
+    has_attached_file :image, styles: { medium: "400x600#" }
+  else
+    has_attached_file :image, styles: { medium: "400x600#" },
+    :storage => :dropbox,
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    :path => ":style/:id_:filename"
+  end
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
