52bb6dc35531cf4f17bf95cc88709967e33247d1
John Park <jwpark417@gmail.com>
2016-05-20 17:25:18 -0400

    Create yeoman scaffold client app

---
+++ meanapp/TrailerPark/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trailerParkApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the trailerParkApp
+ */
+angular.module('trailerParkApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ meanapp/TrailerPark/app/scripts/controllers/movies.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trailerParkApp.controller:MoviesCtrl
+ * @description
+ * # MoviesCtrl
+ * Controller of the trailerParkApp
+ */
+angular.module('trailerParkApp')
+  .controller('MoviesCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
